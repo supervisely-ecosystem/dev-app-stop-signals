e50dab161ef0f410c34d0244431a8ea47d30b421
Maxim Kolomeychenko <maxim@supervise.ly>
2022-11-22 13:06:01 +0000

    done

--- src/main.py
+++ src/main.py
@@ -1,8 +1,40 @@
 import time
+import signal
+import sys
+
+
+def signal_sigint_handler(sig, frame):
+    print("You pressed Ctrl+C!")
+    # sys.exit(0)
+
+
+# no way to handle this
+# def signal_sigkill_handler(sig, frame):
+#     print("You pressed SIGKILL!")
+#     # sys.exit(0)
+
+
+def signal_sigterm_handler(sig, frame):
+    print("You pressed SIGTERM!")
+    # sys.exit(0)
+
+
+def signal_sigusr1_handler(sig, frame):
+    print("You pressed SIGUSR1!")
+
+
+def signal_sigusr2_handler(sig, frame):
+    print("You pressed SIGUSR2!")
+
+
+signal.signal(signal.SIGINT, signal_sigint_handler)
+# signal.signal(signal.SIGKILL, signal_sigkill_handler)
+signal.signal(signal.SIGTERM, signal_sigterm_handler)
+signal.signal(signal.SIGUSR1, signal_sigusr1_handler)
+signal.signal(signal.SIGUSR2, signal_sigusr2_handler)
 
 
 def main():
-
     log_limit = 0
     counter = 0
     while True:
