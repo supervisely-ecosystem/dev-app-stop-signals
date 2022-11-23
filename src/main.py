66e552359326d43e02a40101ad4bfdf488cd1312
Maxim Kolomeychenko <maxim@supervise.ly>
2022-11-23 09:45:15 +0000

    events - wip

--- src/main.py
+++ src/main.py
@@ -1,52 +1,39 @@
+import os
 import time
-import signal
-import sys
+from dotenv import load_dotenv
+from art import tprint
+import supervisely as sly
+from async_asgi_testclient import TestClient
+from supervisely.app.fastapi.utils import run_sync
+
+# load ENV variables for debug
+# has no effect in production
+load_dotenv("local.env")
+load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+app = sly.Application()
 
 
-def signal_sigint_handler(sig, frame):
-    print("You pressed Ctrl+C!")
-    # sys.exit(0)
+# def main():
+# for i in range(100):
+#     print(f"Iteration {i}")
+#     time.sleep(1)
 
 
-# no way to handle this
-# def signal_sigkill_handler(sig, frame):
-#     print("You pressed SIGKILL!")
-#     # sys.exit(0)
-
-
-def signal_sigterm_handler(sig, frame):
-    print("You pressed SIGTERM!")
-    # sys.exit(0)
-
-
-def signal_sigusr1_handler(sig, frame):
-    print("You pressed SIGUSR1!")
-
-
-def signal_sigusr2_handler(sig, frame):
-    print("You pressed SIGUSR2!")
-
-
-signal.signal(signal.SIGINT, signal_sigint_handler)
-# signal.signal(signal.SIGKILL, signal_sigkill_handler)
-signal.signal(signal.SIGTERM, signal_sigterm_handler)
-signal.signal(signal.SIGUSR1, signal_sigusr1_handler)
-signal.signal(signal.SIGUSR2, signal_sigusr2_handler)
-
-
+@app.get_server().on_event("startup")
 def main():
-    log_limit = 0
-    counter = 0
-    while True:
-        time.sleep(0.5)
-        counter += 1
-        if counter % 10 == 0:
-            print(f"Counter = {counter}")
-            log_limit += 1
-        if log_limit >= 100:
-            print("App works too long ...")
-            exit(0)
+    # use only for initialization
+    print("Do something before initialization")
 
 
-if __name__ == "__main__":
-    main()
+@app.get_server().on_event("shutdown")
+def stop():
+    print("Do something before stop")
+
+
+client = TestClient(app)
+resp = run_sync(client.get("/"))
+
+# for i in range(100):
+#     print(f"Iteration {i}")
+#     time.sleep(1)
