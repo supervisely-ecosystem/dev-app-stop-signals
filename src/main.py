7688bfea894757fffda65df45382eb028e284b01
Maxim Kolomeychenko <maxim@supervise.ly>
2022-11-23 17:26:50 +0000

    done

--- src/main.py
+++ src/main.py
@@ -4,28 +4,45 @@ from fastapi_utils.tasks import repeat_every
 from supervisely.app.fastapi import run_sync
 import time
 import asyncio
+import functools
+from threading import Thread, Event
 
 app = FastAPI()
 
-# @app.on_event("startup")
-# async def startup_event():
-#     print("----> start")
-#     long_task_sync()
+
+exit_event = Event()
+thread = None
+
+
+@app.on_event("startup")
+def startup_event():
+    global thread
+    print("----> start")
+    # long_task_sync()
+    threaded_function = functools.partial(long_task_sync)
+    thread = Thread(target=threaded_function)
+    thread.start()
 
 
 @app.on_event("shutdown")
 def shutdown_event():
+    exit_event.set()
+    thread.join()
     print("----> stop")
 
 
 def long_task_sync():
     print("long task sync")
     for i in range(100):
+        if i == 10:
+            raise ValueError("test")
         print(f"Iteration {i}")
         time.sleep(1)
+        if exit_event.is_set():
+            return
 
 
-long_task_sync()
+# long_task_sync()
 
 # # # https://fastapi-utils.davidmontague.xyz/user-guide/repeated-tasks/
 # @repeat_every(seconds=1, max_repetitions=1)
